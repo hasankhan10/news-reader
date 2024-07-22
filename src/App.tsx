35c3d2822dc1116fb00125e308e7ced2cfa02e95
mehedi <darkhasan10@gmail.com>
2024-07-23 02:33:28 +0530

    sign up page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Signup from "./pages/Signup"
 
 function App() {
   return (
-    <div className="main w-full min-h-screen bg-slate-500">
-      
+    <div className="main w-full h-screen bg-slate-800">
+      <Signup />
     </div>
   )
 }
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,33 @@
+
+function Signup() {
+  return (
+    <div className="signup w-full h-full flex justify-center items-center">
+        <div className=" w-[350px] h-[400px] bg-gray-100 rounded-md">
+            <div className="upper w-full h-1/4 flex justify-center items-center gap-2 flex-col">
+                <h1 className=" text-3xl font-semibold">Create your Account</h1>
+                <p className=" font-semibold opacity-70">Enter the field bellow to get start</p>
+            </div>
+            <div className="bottom w-full h-3/4">
+                <form className=" w-full h-full" action="" method="">
+                    <div className=" w-full h-fit px-8">
+                        <label className="font-semibold opacity-80 block" htmlFor="name">Full name<span className="text-red-500">*</span></label>
+                        <input className=" w-[90%] h-10 rounded-md px-3 py-1 mb-auto" type="text" name="name" id="name" placeholder="Enter name" required/>
+                        <br />
+                        <label className="font-semibold opacity-80 block" htmlFor="email">Email address<span className="text-red-500">*</span></label>
+                        <input className=" w-[90%] h-10 rounded-md px-3 py-1 mb-auto" type="email" name="email" id="email" placeholder="Enter email" required/>
+                        <br />
+                        <label className="font-semibold opacity-80 block" htmlFor="password">Password<span className="text-red-500">*</span></label>
+                        <input className=" w-[90%] h-10 rounded-md px-3 py-1 mb-auto" type="password" name="password" id="password" placeholder="Create password" required/>
+                    </div>
+                    <div className=" w-full flex justify-center items-center mt-5">
+                        <button className=" w-4/5 h-8 bg-blue-500 rounded-md text-white font-semibold" type="submit">Create account</button>
+                    </div>
+                    <p className=" w-full flex justify-center items-center gap-2 mt-2 opacity-70 font-semibold">Already have an account? <a className=" text-blue-800 font-bold opacity-100" href="">Login</a></p>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
